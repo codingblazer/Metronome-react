28f3d9437248d3e07db424b5a413dd5617e6cdbd
Sachin Aggarwal <sasachin04@gmail.com>
2019-04-04 08:27:42 +0530

    React state and event handling in form

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+ state = {firstname: ''};
+
+  handleFormChange = (e) => {
+    const name = e.target.value;
+    this.setState({firstname: name});
+    console.log(this.state.firstname)
+  }
+
   render() {
+    console.log('render called');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>First Name: {this.state.firstname}</div>
+        <div className="form-group">
+            <label htmlFor="firstName">First Name: </label>
+            <input type="text" name="firstName" onChange={this.handleFormChange} />
+        </div>
       </div>
     );
   }
